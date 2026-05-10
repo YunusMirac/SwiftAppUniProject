bf14b571846ba1caa68e7d396c3276020a99eaff
yunus <yunusmiracomart@gmail.com>
2026-05-10 21:36:09 +0200

    Initial Commit

---
+++ MemoSpiel/MemoSpielApp.swift
@@ -0,0 +1,17 @@
+//
+//  MemoSpielApp.swift
+//  MemoSpiel
+//
+//  Created by Yunus Mirac Comart on 10.05.26.
+//
+
+import SwiftUI
+
+@main
+struct MemoSpielApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
